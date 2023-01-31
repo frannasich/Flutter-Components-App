8a14181b59cb1a2b5c190b1d569d8bdf0b436f5e
Francisco <francisco.nasich@mercadolibre.com>
2023-01-31 13:15:32 -0300

    some visual changes

--- lib/screens/list_view2.dart
+++ lib/screens/list_view2.dart
@@ -12,14 +12,23 @@ class ListView2Screen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color amberColor = Colors.amber.shade600;
+
     return Scaffold(
         appBar: AppBar(
           title: const Text('List View 2'),
+          backgroundColor: amberColor,
+          elevation: 0,
         ),
         body: ListView.separated(
             itemBuilder: (context, index) => ListTile(
                   title: Text(options[index]),
-                  trailing: const Icon(Icons.keyboard_arrow_right_rounded),
+                  trailing: Icon(Icons.keyboard_arrow_right_outlined,
+                      color: amberColor),
+                  onTap: () {
+                    final game = options[index];
+                    print(game);
+                  },
                 ),
             separatorBuilder: (_, __) => const Divider(),
             itemCount: options.length));
